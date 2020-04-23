a693525670dd9faadf7a34c214e7f581ff5b39a9
Daniele da Conceição <daninha.conceicao1980@gmail.com>
2020-04-23 00:03:39 -0300

    Atualizando e adicionando
    Atualizando footer e header e adicionando o menu hamburger

--- petslovernv/resources/views/template/template.blade.php
+++ petslovernv/resources/views/template/template.blade.php
@@ -3,8 +3,8 @@
 <head>
   <title>Pet's Lover</title>
   
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf_token" content="{{ csrf_token() }}">
   <link rel="stylesheet" type="text/css" href="{{ asset ('css/style.css') }} ">
   <link rel="stylesheet" type="text/css" href="{{ asset ('css/normalize.css') }} ">
@@ -20,6 +20,8 @@
 <!-- tentei inserir essa funcao no outro arquivo, mas nao deu certo. Deve ser porque utiliza o atributo name do href contido na pag e nao chama pelo nome da funcao. 
 Para deixar sem esse trecho de cod aqui, será necessario utilizar outra funcao de modal. Ass:D-->
 <script type="text/javascript">
+
+/* utilizado no modal do link nao possuo cadastro e no bt entrar do header */
 
 $(document).ready(function() {  
 
@@ -61,6 +63,20 @@ $(document).ready(function() {
   
 });
 
+/* Utilizado para exibir o menu apenas nas medias 6 e 7 */
+    $("#menu").click(function(e) {
+        e.preventDefault();
+        $("#divMen").toggleClass("toggled");
+    });
+/* Utilizado no menu hamburguer ao exibir os links */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "90%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
 </script> 
 
    <header>
@@ -71,37 +87,46 @@ $(document).ready(function() {
                 </div> 
                 <div class="acesso acessoDef" style="width:74.128984%"> 
                     
-                    @if(Session::has('usuario'))
+                  @if(Session::has('usuario'))
+		    <div class="MLogUs">  
                      <p align="right" class="pOla">Olá</p>                        
                      <p align="right">
-                            <a href="logout.php" id="senha" class="linkOut" tabindex="2">SAIR</a>
-                      </p>
-                           
-                    @else
-
+                         <a href="logout.php" id="senha" class="linkOut" tabindex="2">SAIR</a>
+                     </p>
+                    </div>       
+                  @else
+                      <div class="divlogin" id="divlogin">
                        <form id="login" name="login" method="POST" action="/logar">
-                        {{csrf_field()}}
-                           <div class="divlogin"> 
+                        {{csrf_field()}} 
                               <p align="right">
                                   <input type="email" class="inpsize" id="email" name="email" maxlength="60" tabindex="1" size="36" required placeholder="E- mail" required> 
                                   <input type="password" id="pass" name="senha" required class="inpsize" maxlength="20" tabindex="2" placeholder="Senha">&nbsp;
-                                  <input type="submit"  name="btlogin" type="button" id="btlogin" tabindex="3" value="Entrar"/>
+                                  <input type="submit"  name="btlogin" type="button" id="btlogin" tabindex="3"value="Entrar"/>
                               </p>
                                
                               <p class="llogin lNao" style="width:53%;">
                                 <a href="/form-recuperar-senha" id="senha" tabindex="4">  Esqueci a Senha </a>
                                 <a href="#dialog" name="modal" id="senha" tabindex="5"> &nbsp;&nbsp;&nbsp; N&atilde;o Possuo Cadastro</a>
-                              </p><div id="hidden" align="right">&nbsp;</div>
-                              
-                           </div>  
-                                
-                       </form>
-                    @endif
-                
-        </div></div>
+                              </p><div id="hidden" align="right">&nbsp;</div>                                         
+                        </form>
+		       </div> <!-- fim divlogin -->
+                  @endif
+		  
+                   <!-- div menu que exibe as informacoes do menu hamb. -->
+                    <div id="divMen" class="divMenu">
+                      <span href="#menu" class="spanMenu" onclick="openNav()">&#9776;</span>
+                  @if(Session::has('usuario'))
+                  @else
+                      <a href="#log" id="btlogin" style="float:right" class="llog" name="modal" tabindex="3" >Entrar</a>
+                  @endif             
+                    </div><!-- fim div menu --> 
+                  
+        </div> <!-- fim div acesso -->
+      </div>
+        
         <nav id="headernav">
           <div align="center">
-            <ul>
+            <ul id="mainNav">
             @if(Session::has('usuario'))
               <li><a href='/perfil'>Principal</a></li>
               <li><a href="/galeria">Galeria</a></li>
@@ -117,37 +142,97 @@ $(document).ready(function() {
          
         </nav>
     </div>
+    
+     <!-- Exibe o conteúdo do menu hamburguer -->
+      <div id="mySidenav" class="sidenav">
+          
+          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+          <a href="index.php" style="padding-left: 14px"><img src="{{ asset('image/logo.png') }}" ></a>
+          <br>
+          <nav id="headernav">
+          <ul style="padding-left: 20px">
+	    @if(Session::has('usuario'))
+              <li><a href='/perfil'>Principal</a></li>
+              <li><a href="/galeria">Galeria</a></li>
+              <li><a href='/contato'>Fale Conosco</a></li>
+	      <li><a href="quem-somos">Quem Somos</a></li>
+	      <li><a href="termo-de-uso">Termos de Compromisso</a></li>
+	      <li><a href="perguntas-frequentes">Perguntas Frequentes</a></li>
+	    @else   
+	      <li><a href='/'>Principal</a></li>
+              <li><a href='/galeria'>Galeria</a></li>
+              <li><a href='/cadastrar'>Cadastro</a></li>
+              <li><a href='/contato'>Fale Conosco</a></li>
+	      <li><a href="quem-somos">Quem Somos</a></li>
+	      <li><a href="termo-de-uso">Termos de Compromisso</a></li>
+	      <li><a href="perguntas-frequentes">Perguntas Frequentes</a></li>
+	  </ul>
+          </nav>
+        </div>
+    <!--  fim menu hamburguer -->
     <script src="{{ asset('js/jquery-1.12.3.js') }}"></script>
     <script src="{{ asset('js/functions.js') }}"></script>
     
-</header>
+  <!-- Exibe o modal login  no bt entrar-->
  
+  <div id="boxes1" >
+     <div id="log" class="window">
+       <form id="login" name="login" method="POST" action="verifica_login.php">
+           
+            <br> <a href="#" class="close" style="">X &nbsp;&nbsp;</a> 
+               
+                      <p align="center">
+                          <input type="email" class="recsenha" id="email" name="email" maxlength="60" tabindex="1"  required placeholder="E - mail" required></p> 
+                      <p align="center" >
+                          <input type="password" id="pass" name="senha" required class="recsenha" maxlength="20" tabindex="2" placeholder="Senha"></p>
+                      <p align="center">
+                          <input type="submit" name="btlogin" type="button" id="btlogin" tabindex="3" value="Entrar"/></p>
+                      <br>
+                               
+                      <p align="center" style="margin:auto">
+                          <a href="form_recuperar_senha.php" id="mLog" tabindex="4">  Esqueci a Senha</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                          <a href="cadastro.php" id="mLog" tabindex="5">  N&atilde;o Possuo Cadastro</a>
+                      </p><br>
+                      <div id="hidden"></div>
+       </form>
+    </div>
+
+   <!-- Máscara para cobrir a tela -->
+    <div id="mask"></div> 
+  
+   <!-- Fim do modal login-->   
+    
+</header>
+
+<!-- Exibe o modal com os bts doar e adotar -->
 <div id="boxes">
      <div id="dialog" class="window trocaDiv linksModal">
+       <br> <a href="#" class="close">X &nbsp;&nbsp;</a> 
        <ul>
-        <li class="li1Modal"><a href="cadastrar-adotante" onclick="this.value=''"> Quero Adotar </a></li> 
-        <li class="li2Modal"><a href="cadastrar" onclick="this.value=''"> Quero Doar </a></li> 
+        <li class="li1Modal"><a href="cadastrar" onclick="this.value=''"> Quero Doar </a></li> 
+        <li class="li2Modal"><a href="cadastrar-adotante" onclick="this.value=''"> Quero Adotar </a></li> 
        </ul>  
      </div>
 </div>
+<!--  fim do modal  bts doar e adotar --> 
 <!-- ================================================ -->
 
 	@yield('content')
 
 <footer>
 	<div id="footer">
-	  	<nav id="footernav">
+	     <nav id="footernav">
 	    	<div align="center">
-	  			<ul>
-	  				<li><a href="quem-somos">Quem Somos</a></li>
-	  				<li><a href="termo-de-uso">Termos de Compromisso</a></li>
-	  				<li><a href="perguntas-frequentes">Perguntas Frequentes</a></li>
+	  			<ul id="mainNav">
+	  		          <li><a href="quem-somos">Quem Somos</a></li>
+	  		          <li><a href="termo-de-uso">Termos de Compromisso</a></li>
+	  		          <li><a href="perguntas-frequentes">Perguntas Frequentes</a></li>
 	  		 	</ul><br>
-	       	<p align="center" class="sAssinatura">&copy; 2016 Pet's Lover - Todos os direitos reservados.</p>
+	       	  <p align="center" class="sAssinatura">&copy; 2016 Pet's Lover - Todos os direitos reservados.</p>
 	       
-	  	  </div>
+	       </div>
 	    </nav>
-	</div>
+        </div>
 </footer>
 </body>
 </html>
